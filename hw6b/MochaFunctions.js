267e162be7be6efc6f4910ff4599c1b2d96fef03
mhaddis <54864661+mhaddis@users.noreply.github.com>
2020-01-21 16:16:59 -0600

    Update MochaFunctions.js

--- hw6b/MochaFunctions.js
+++ hw6b/MochaFunctions.js
@@ -1,16 +1,25 @@
-
-function maxOfTwo(num1, num2){
-    if (num1>num2) return num1;
-    else return num2;
+/*jshint esversion: 6 */
+(function (){
+    'use strict'
+    function maxOfTwo(num1, num2)
+    {
+    if (num1>num2){
+        return num1;
+    }
+    else {return num2;}
 }
 
 function maxOfThree(num1, num2, num3) {
     if (num1 > num2) {
-        if (num1 > num3) return num1;
+        if (num1 > num3) {
+            return num1;
+        }
     } else {
-        if (num2 > num3) return num2;
-        else
+        if (num2 > num3) {
+            return num2;
+        } else {
             return num3;
+        }
     }
 }
 
@@ -58,35 +67,36 @@ function findLongestWord(str) {
 
 function filterLongWords(str, num) {
     for (let i=0; i<str.length;i++){
-        if(str[i].length>num) return str[i];
+        if(str[i].length>num) {
+            return str[i];
+        }
     }
 }
 
-
 describe("MochaFunctions", function() {
-    context("when 2 and 3 entered", function() {
-        beforeEach(function() {
-        it("maxNumber is 15", function() {
-            assert.equal(maxOfTwo(15, 10), 15);
-        });
-        it("maxNumber is 15", function() {
-            assert.equal(maxOfThree(15, 10,5), 15);
-        });
-        it("isVowel(S) is false", function() {
-            assert.equal(isVowel('s'), false);
-        });
-        it("sum for numbers(1,2,3,4) is 10", function() {
-            assert.equal(sumNum([1,2,3,4]), 10);
-        });
-        it("product for numbers(1,2,3,4)  is 24", function() {
-            assert.equal(productNum([1,2,3,4]), 24);
-        });
-        it("reverse str(Hello) is olleH", function() {
-            assert.equal(reverse("Hello"), "olleH");
-        });
-        it("output of findLongestWord(Hello,Hi,Hey) is 5", function() {
-            assert.equal(findLongestWord('olleH'), 5);
-        });
-        it("LongestWord is Hello", function() {
-            assert.equal(filterLongWords(["Hello", "Hi", "Hey)"]), 3);
-        });
+    it("maxNumber is 15", function () {
+        assert.equal(maxOfTwo(15, 10), 15);
+    });
+    it("maxNumber is 15", function () {
+        assert.equal(maxOfThree(15, 10, 5), 15);
+    });
+    it("isVowel(S) is false", function () {
+        assert.equal(isVowel('s'), false);
+    });
+    it("sum for numbers(1,2,3,4) is 10", function () {
+        assert.equal(sumNum([1, 2, 3, 4]), 10);
+    });
+    it("product for numbers(1,2,3,4)  is 24", function () {
+        assert.equal(productNum([1, 2, 3, 4]), 24);
+    });
+    it("reverse str(Hello) is olleH", function () {
+        assert.equal(reverse("Hello"), "olleH");
+    });
+    it("output of findLongestWord(Hello,Hi,Hey) is 5", function () {
+        assert.equal(findLongestWord(["Hello", "Hi", "Hey"]), 5);
+    });
+    it("LongestWord is Hello", function () {
+        assert.equal(filterLongWords(["Hello", "Hi", "Hey"],3), "Hello");
+    });
+});
+}());
